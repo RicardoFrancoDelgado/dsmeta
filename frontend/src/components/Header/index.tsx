88d29195f5a25037faa1e2ab6150dde7c04440d7
Ricardo Franco Delgado <rf041555@gmail.com>
2022-11-14 19:27:47 -0300

    Changes description

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -8,14 +8,7 @@ function Header() {
           <img src={logo} alt="DSMeta" />
           <h1>DSMeta</h1>
           <p>
-            Desenvolvido por
-            <a href="https://www.instagram.com/devsuperior.ig">
-              @devsuperior.ig
-            </a>
-            <span> & </span>
-            <a href="https://www.instagram.com/riicardiinn/">@riicardiinn</a>
-            <span> </span>
-            na Semana React Spring Devsuperior
+            Desenvolvido na Semana React Spring Devsuperior
           </p>
         </div>
       </header>
