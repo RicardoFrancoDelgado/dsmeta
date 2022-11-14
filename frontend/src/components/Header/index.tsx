9979938274de77274ef5160ffc03ed6c4a106894
Ricardo Franco Delgado <rf041555@gmail.com>
2022-11-14 19:25:11 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,9 +1,8 @@
-import logo from '../../assets/img/logo.svg'
-import './style.css'
+import logo from '../../assets/img/logo.svg';
+import './styles.css';
 
 function Header() {
   return (
-    <>
       <header>
         <div className="dsmeta-logo-container">
           <img src={logo} alt="DSMeta" />
@@ -20,8 +19,7 @@ function Header() {
           </p>
         </div>
       </header>
-    </>
   )
 }
 
-export default Header
+export default Header;
