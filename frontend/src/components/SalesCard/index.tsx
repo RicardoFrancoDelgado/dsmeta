63d20ee30438301b347b5f862e31a57c469129f6
Ricardo Franco Delgado <rf041555@gmail.com>
2022-11-18 22:19:36 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -18,10 +18,9 @@ function SalesCard() {
 
   
   useEffect(() => {
-    const dmin = minDate.toISOString().slice(0, 10)
-    const dmax = minDate.toISOString().slice(0, 10)
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = minDate.toISOString().slice(0, 10);
     
-    console.log(dmin)
 
     axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
       .then(response => {
@@ -78,7 +77,7 @@ function SalesCard() {
                   <td>R$ {sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
-                      <NotificationButton />
+                      <NotificationButton saleId={sale.id} />
                     </div>
                   </td>
                 </tr>
